1b6f6628bfe95e09d3d385f42ffad49588345312
umwalikara <umwaliclara@gmail.com>
2019-09-30 08:39:13 +0200

    adding functionarity in request file

--- app/request.py
+++ app/request.py
@@ -7,16 +7,16 @@ from config import Config
 
 # def configure_request(app):
 #     global base_url
-# base_url = Config.QUOTES_API_BASE_URL
+base_url = Config.QUOTES_API_BASE_URL
 
-# def get_quotes():
-#     '''
-#     function that gets json response to our url request
-#     '''
-#     quote_object = requests.get(base_url) #to get quote url
-#     new_quote = quote_object.json() # in getting objects in json format
-#     author = new_quote.get("author")
-#     quote = new_quote.get("quote")
-#     object = Quotes(author,quote)
+def get_quotes():
+    '''
+    function that gets json response to our url request
+    '''
+    quote_object = requests.get(base_url) #to get quote url
+    new_quote = quote_object.json() # in getting objects in json format
+    author = new_quote.get("author")
+    quote = new_quote.get("quote")
+    object = Quotes(author,quote)
 
-#     return object
+    return object

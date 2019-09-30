93bd014aff9a1a81d10f36f6052fc3fcb27ce6a3
umwalikara <umwaliclara@gmail.com>
2019-09-30 09:02:54 +0200

    adding login function in views file

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,19 +8,19 @@ from ..email import mail_message
 
 
 
-# @auth.route('/login',methods=['GET','POST'])
-# def login():
-#     login_form = LoginForm()
-#     if login_form.validate_on_submit():
-#         user = User.query.filter_by(email = login_form.email.data).first()
-#         if user is not None and user.verify_password(login_form.password.data):
-#             login_user(user,login_form.remember.data)
-#             return redirect(request.args.get('next') or url_for('main.index'))
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-#         flash('Invalid username or Password')
+        flash('Invalid username or Password')
 
-#     title = "blog login"
-#     return render_template('auth/login.html',login_form = login_form,title=title)
+    title = "blog login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
 # @auth.route('/register',methods = ["GET","POST"])
 # def register():

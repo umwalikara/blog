8fb0421c9913565f058eac3c544dc4025a8da73c
umwalikara <umwaliclara@gmail.com>
2019-09-30 09:04:41 +0200

    adding registration form class in form file

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,20 +5,20 @@ from wtforms import ValidationError
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
 
-# class RegistrationForm(FlaskForm):
-#     email = StringField('Your Email Address',validators=[Required(),Email()])
-#     username = StringField('Enter your username',validators = [Required()])
-#     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
-#     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
-#     submit = SubmitField('Sign Up')
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
 
-#     def validate_email(self,data_field):
-#             if User.query.filter_by(email =data_field.data).first():
-#                 raise ValidationError('There is an account with that email')
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
 
-#     def validate_username(self,data_field):
-#         if User.query.filter_by(username = data_field.data).first():
-#             raise ValidationError('That username is taken')
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
 
 # class LoginForm(FlaskForm):
 #     email = StringField('Your Email Address',validators=[Required(),Email()])

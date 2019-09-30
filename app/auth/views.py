73a14611104927feb9d41ab0431118dc1699fd10
umwalikara <umwaliclara@gmail.com>
2019-09-30 09:02:23 +0200

    adding importation in views file

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,10 @@
-# from flask import render_template,redirect,url_for,flash,request
-# from . import auth
-# from flask_login import login_user,logout_user,login_required
-# from ..models import User
-# from .forms import LoginForm,RegistrationForm
-# from .. import db
-# from ..email import mail_message
+from flask import render_template,redirect,url_for,flash,request
+from . import auth
+from flask_login import login_user,logout_user,login_required
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from .. import db
+from ..email import mail_message
 
 
 

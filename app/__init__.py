a69cec623bbe51fc190aa724d67d4834b2a93b23
umwalikara <umwaliclara@gmail.com>
2019-09-30 08:43:59 +0200

    adding importation in init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,25 @@
-# from flask import Flask
-# from flask_bootstrap import Bootstrap
-# from config import config_options
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
-# from flask_uploads import UploadSet,configure_uploads,IMAGES
-# from flask_mail import Mail
-# from flask_simplemde import SimpleMDE
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
-# photos = UploadSet('photos',IMAGES)
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
-# bootstrap = Bootstrap()
-# db = SQLAlchemy()
-# mail = Mail()
-# simple = SimpleMDE()
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+simple = SimpleMDE()
 
-# def create_app(config_name):
+def create_app(config_name):
 
-#     app = Flask(__name__)
+    app = Flask(__name__)
 
     # Initializing flask extensions
     bootstrap.init_app(app)

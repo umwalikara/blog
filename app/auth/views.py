3d0ad5e21b1dca6e48da48c3583e1f779e1c827b
umwalikara <umwaliclara@gmail.com>
2019-09-30 09:03:32 +0200

    adding logout and register function in views file

--- app/auth/views.py
+++ app/auth/views.py
@@ -22,21 +22,21 @@ def login():
     title = "blog login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
-# @auth.route('/register',methods = ["GET","POST"])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
 
-#         # mail_message("Welcome to pitch","email/welcome_user",user.email,user=user)
-#         return redirect(url_for('auth.login'))
-#         title = "New Account"
-#     return render_template('auth/register.html',registration_form = form)
+        # mail_message("Welcome to pitch","email/welcome_user",user.email,user=user)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
-# @auth.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for("main.index"))
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))

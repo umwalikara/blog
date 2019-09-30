9e00d5b0e85472f41fb879168b66ee6b29e4bfa3
umwalikara <umwaliclara@gmail.com>
2019-09-30 09:03:59 +0200

    adding importation in form file

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,8 @@
-# from flask_wtf import FlaskForm
-# from wtforms.validators import Required,Email,EqualTo
-# from ..models import User
-# from wtforms import ValidationError
-# from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from flask_wtf import FlaskForm
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+from wtforms import ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
 
 # class RegistrationForm(FlaskForm):
